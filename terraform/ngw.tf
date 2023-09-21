f629eb9d6229eee11b32274215e9e5a7cc50b7c7
xuyang0602 <wang.xuyang0602@gmail.com>
2023-09-21 14:33:20 -0700

    Add igw & ngw

---
+++ terraform/ngw.tf
@@ -0,0 +1,21 @@
+resource "aws_nat_gateway" "ngw1" {
+  allocation_id = ...
+  subnet_id = aws_subnet.public_1.id
+
+  depends_on = [ aws_internet_gateway.igw ]
+
+  tags = {
+    Name = "${var.app_name}-ngw1"
+  }
+}
+
+resource "aws_nat_gateway" "ngw2" {
+  allocation_id = ...
+  subnet_id = aws_subnet.public_2.id
+
+  depends_on = [ aws_internet_gateway.igw ]
+
+  tags = {
+    Name = "${var.app_name}-ngw2"
+  }
+}
